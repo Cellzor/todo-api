3029a43a455962f6240fabf4f21e4e7493547db8
Christian Kuhr <Christian@noplay.com>
2016-08-21 18:24:11 +0200

    Add postgren for production

--- db.js
+++ db.js
@@ -2,10 +2,22 @@
  * Created by Christian on 2016-08-21.
  */
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect': 'sqlite',
-    'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if(env === 'production') {
+    //only run on heroku
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres'
+    });
+} else {
+    //in dev env
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/dev-todo-api.sqlite'
+    });
+}
+
 
 var db = {};
 
